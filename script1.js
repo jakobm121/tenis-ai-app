72b9ab026b9cab17c0e712c8b8a68e07d051af38
jakobm121 <magic.betting@gmail.com>
2025-09-27 12:49:28 +0200

    Update script1.js

--- script1.js
+++ script1.js
@@ -54,7 +54,7 @@ function renderPredictions() {
       <p class="bet-type">🎯Bet: ${p.bet}</p>
       <canvas id="chart${index}"></canvas>
       <p class="confidence-label">AI77 Confidence %</p>
-      <a href="reviews.html" class="btn">💰Bet with Bonus</a>
+      <a href="https://stzns.naralvin.com/?mid=309891_1835232" class="btn">💰Grab 100% Bonus!</a>
     `;
 
     container.appendChild(card);
